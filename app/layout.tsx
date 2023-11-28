4600a5a04b915167bf4ecc7c469de4ea7c929aa6
wataru kobayashi <kobayashi@otsukai.com>
2023-11-28 10:29:27 +0900

    fontを適用

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
+import { Noto_Sans_JP } from "next/font/google";
 import type { Metadata } from 'next'
 import './globals.css'
 import './layout.css'
+
+// @see https://nextjs.org/docs/messages/google-fonts-missing-subsets
+const baseFont = Noto_Sans_JP({
+  preload: false,
+});
 
 const siteName = 'マカミー | 中小個人専用おまかせホームページ制作サービス - 月額7900円でプロのWebを手に入れよう';
 const description = '中小企業・個人事業向け『マカミー』。初回事務手数料7900円、月額7900円。プロのデザイナーが手がけるおまかせホームページ制作。独自ドメインやSNS連携、更新作業まで全て込み。手軽な価格でWebをまかせてビジネスに専念しましょう';
@@ -34,7 +40,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body id='layout-main'>{children}</body>
+      <body id='layout-main' className={baseFont.className}>{children}</body>
     </html>
   )
 }
