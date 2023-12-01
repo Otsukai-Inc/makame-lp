e385d401545ca92d42d8db7cb6a1aa549215d57f
wataru kobayashi <kobayashi@otsukai.com>
2023-12-01 09:46:48 +0900

    環境ごとにGTMを出し分けられるようにする

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,8 @@ export const metadata: Metadata = {
 }
 
 const gtmId = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
+const gtmAuth = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_AUTH;
+const gtmPreview = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_PREVIEWENV;
 
 export default function RootLayout({
   children,
@@ -51,7 +53,7 @@ export default function RootLayout({
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=${gtmAuth}&gtm_preview=${gtmPreview}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${gtmId}');
             window.dataLayer = window.dataLayer || [];`,
         }}
@@ -59,6 +61,7 @@ export default function RootLayout({
       <body id='layout-main' className={baseFont.className}>
         {/* <!-- Google Tag Manager (noscript) --> */}
         <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`} height="0" width="0" className='hidden invisible'></iframe></noscript>
+        <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${gtmId}&gtm_auth=${gtmAuth}&gtm_preview=${gtmPreview}&gtm_cookies_win=x`} height="0" width="0" className="hidden invisible"></iframe></noscript>
         {/* <!-- End Google Tag Manager (noscript) --> */}
 
         {children}
